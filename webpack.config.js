9f38c3b578ec595d539fa50fe8a02d12e149ca95
RedShucov <redshucov@gmail.com>
2023-01-16 07:32:33 +0300

    Добавляет установку и настройку транспайлера babel

--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,13 @@ module.exports = {
       patterns: [{ from: 'public' }],
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: ['babel-loader']
+      }
+    ]
+  }
 };
-
-// 3) Добавьте в package.json скрипт с именем build со значением webpack --mode production. С его помощью мы будем собирать Webpack'ом код для публикации проекта.
-
-// 4) Собираемые с помощью Webpack файлы не должны попасть в репозиторий, поэтому добавьте их в .gitignore.

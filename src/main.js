138ccd6e62ed6e84de53371fb8c77b85a6e197fa
RedShucov <redshucov@gmail.com>
2023-02-01 20:52:14 +0300

    Добавит презентер поездки

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import WaypointsFilterView from './view/waypoints-filter-view.js';
 import { render } from './render.js';
+import TripPresenter from './presenter/trip-presenter.js';
 
 const siteFiltersElement = document.querySelector('.trip-controls__filters');
+const siteTripBody = document.querySelector('.trip-events');
+
+const tripPresenter = new TripPresenter(siteTripBody);
 
 render(new WaypointsFilterView(), siteFiltersElement);
+
+tripPresenter.init();
